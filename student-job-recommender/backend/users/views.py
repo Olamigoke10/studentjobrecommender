7c891ab21a1df56101aac7660457bfd71fdb015d
MRBOX <olamigokeadebayo90@gmail.com>
2026-02-15 18:59:48 +0000

    Register serializer

--- student-job-recommender/backend/users/views.py
+++ student-job-recommender/backend/users/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics,permissions
-from .serializers import RegisterSerializer, StudentProfileSerializer
+from .serializers import RegisterSerializer, StudentProfileSerializer, User
 from .models import StudentProfile
 from .token import EmailTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-
 class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()  
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
+
     
 
 class StudentProfileView(generics.RetrieveUpdateAPIView):
