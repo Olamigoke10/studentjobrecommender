d02d757cfbfe8858110adb3fd549678b6f75b783
MRBOX <olamigokeadebayo90@gmail.com>
2026-02-15 16:27:55 +0000

    EmailToken Inherit TokenObtain

--- student-job-recommender/backend/users/token.py
+++ student-job-recommender/backend/users/token.py
@@ -1,23 +1,7 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth import authenticate
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
-class EmailTokenObtainPairSerializer(TokenObtainPairSerializer):
-    
+class EmailTokenObtainPairSerializer(TokenObtainPairSerializer):    
     username_field = 'email'
-    
-    def validate(self, attrs):
-        credentials = {
-            "email": attrs.get("email"),
-            "password": attrs.get("password")
-        }
-        
-        user = authenticate(**credentials)
-        
-        if not user:
-            raise Exception("Invalid email or password")
-        
-        
-        data = super().validate(attrs)
-        data["email"] = user.email
-        return data
